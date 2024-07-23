7071a2fc411cdd65bf3a3bea95ec96a59d0f042d
lucy1287 <lucy1287@naver.com>
2024-07-23 13:49:56 +0900

    Feat: 페이퍼에 글 작성한 사람 조회

--- routes/paper.js
+++ routes/paper.js
@@ -2,8 +2,11 @@
 const express = require('express');
 const router = express.Router();
 const { getPapersByGroupNo } = require('../controllers/paper');
+const { getLettersWithUserInfo } = require('../controllers/paper');
 
 // 그룹의 롤링페이퍼 조회
 router.get('/', getPapersByGroupNo);
+
+router.get('/userInfo', getLettersWithUserInfo);
 
 module.exports = router;
